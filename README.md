6491b53e245860974657684ae7a1cf906316e314
WhySoPowerful <rafidshaon@gmail.com>
2023-03-31 00:53:43 -0400

    initial documentation setup

---
+++ README.md
@@ -0,0 +1,27 @@
+# IdenTweety - Twitter Bot Detector
+IdenTweety is a Twitter bot detection app created using Python, powered by Streamlit, Tweepy, and Botometer libraries. The app provides a user-friendly interface to input a Twitter handle and analyze the account for bot-like behavior. It retrieves user information such as profile picture, name, description, and follower/following count. IdenTweety also computes bot scores for the input account and its followers using Botometer's machine learning algorithms. This helps users identify potentially automated accounts or disinformation campaigns on Twitter.
+
+To set up and use the app, follow the instructions in the readme.md file, which guides you through installing required dependencies, obtaining API keys from Botometer and Twitter, and running the app locally using Streamlit. With IdenTweety, you can conveniently assess the authenticity of Twitter accounts and gain insights into their followers' bot scores.
+## Requirements
+Requires Python 3.8+.
+## Dependencies
+- botometer
+- pandas
+- streamlit
+- tweepy
+## Setup
+1. `pip install -r requirements.txt`
+2. Follow the following instructions to [Generate an API Key for Botometer](https://rapidapi.com/OSoMe/api/botometer-pro/details)
+3. Follow the following instructions to [Generate an API Key for Twitter](https://developer.twitter.com/en/docs/twitter-api/getting-started/getting-access-to-the-twitter-api)
+4. Modify `app.py` file with your API Key information
+> **Note:** You will see the `# Authenticate with Twitter API and Botometer` comment and you fill out everything that says **KEY HERE**... example below ->
+>`rapidapi_key = "YOUR RAPID API KEY HERE"`
+>`twitter_app_auth = {`
+>    `'consumer_key': 'YOUR CONSUMER KEY HERE',`
+>    `'consumer_secret': 'YOUR CONSUMER SECRET HERE',`
+>    `'access_token': 'YOUR ACCESS TOKEN HERE',`
+>    `'access_token_secret': 'YOUR ACCESS TOKEN SECRET HERE',`
+>`}`
+## Run
+- From inside of the root folder: `streamlit run app.py`
+
---
+++ app.py
@@ -0,0 +1,130 @@
+import streamlit as st
+import tweepy
+import botometer
+
+# Set page layout
+st.set_page_config(
+    page_title='IdenTweety',
+    page_icon=':bird:',
+    layout='wide',
+    initial_sidebar_state='expanded'
+)
+
+# Set app background color
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #F9F9F9;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Set app logo and title
+def add_logo():
+        st.markdown(
+        """
+        <style>
+            [data-testid="stSidebarNav"] {
+                background-image: url(https://i.imgur.com/GGVshv0.png);
+                background-repeat: no-repeat;
+                padding-top: 50px;
+                background-size: contain;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+st.markdown("<h1 style='text-align: center; color: #33C5FF;'>Bot or Not?</h1>", unsafe_allow_html=True)
+
+# Call function to add logo
+add_logo()
+
+# Authenticate with Twitter API and Botometer
+rapidapi_key = "YOUR RAPID API KEY HERE"
+twitter_app_auth = {
+    'consumer_key': 'YOUR CONSUMER KEY HERE',
+    'consumer_secret': 'YOUR CONSUMER SECRET HERE',
+    'access_token': 'YOUR ACCESS TOKEN HERE',
+    'access_token_secret': 'YOUR ACCESS TOKEN SECRET HERE',
+}
+bom = botometer.Botometer(
+    wait_on_ratelimit=True,
+    rapidapi_key=rapidapi_key,
+    **twitter_app_auth
+)
+auth = tweepy.OAuthHandler(
+    twitter_app_auth['consumer_key'],
+    twitter_app_auth['consumer_secret']
+)
+auth.set_access_token(
+    twitter_app_auth['access_token'],
+    twitter_app_auth['access_token_secret']
+)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+# Create page sections
+header = st.container()
+input_section = st.container()
+output_section = st.container()
+
+# Set header section
+with header:
+    st.markdown("<hr style='border: 1px solid #33C5FF; border-radius: 2px;'>", unsafe_allow_html=True)
+
+# Set input section
+with input_section:
+    st.markdown("<h2 style='color: #33C5FF;'>Enter a Twitter Handle</h2>", unsafe_allow_html=True)
+    user_input = st.text_input('', '')
+    if st.button('Identify'):
+        if user_input:
+            if user_input[0] == '@':
+                user_input = user_input[1:]
+            try:
+                user = api.get_user(user_input)
+                profile_image_url = user.profile_image_url.replace('_normal', '')
+                followers_count = user.followers_count
+                friends_count = user.friends_count
+                st.image(profile_image_url, width=200)
+                st.markdown(f"<h3 style='color: var(--main-text-color);'>@{user_input}</h3>", unsafe_allow_html=True)
+                st.markdown(f"<p style='color: var(--secondary-text-color);'>{user.name}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p style='color: var(--secondary-text-color);'>{user.description}</p>", unsafe_allow_html=True)
+                st.markdown("<hr style='border: 1px solid #33C5FF; border-radius: 2px;'>", unsafe_allow_html=True)
+                st.write('')
+                col1, col2, col3 = st.columns([1,2,1])
+                col2.markdown("<h3 style='text-align: center; color: #33C5FF;'>Stats</h3>", unsafe_allow_html=True)
+                col1.markdown(f"<h4 style='text-align: center; color: var(--main-text-color);'>Followers: <span style='color: #00FF00;'>{followers_count}</span></h4>", unsafe_allow_html=True)
+                col3.markdown(f"<h4 style='text-align: center; color: var(--main-text-color);'>Following: <span style='color: #00FF00;'>{friends_count}</span></h4>", unsafe_allow_html=True)
+
+
+                follower_screen_names = [f'@{follower.screen_name}' for follower in tweepy.Cursor(api.followers, user_input).items(20)]
+                follower_screen_names_bot_scores = {}
+                for screen_name, result in bom.check_accounts_in(follower_screen_names):
+                    try:
+                        scores = list(result['raw_scores']['english'].values())
+                        bot_score = round(sum(scores) / len(scores), 2)
+                        follower_screen_names_bot_scores[screen_name] = bot_score
+                    except Exception:
+                        follower_screen_names_bot_scores[screen_name] = 'NaN'
+
+                result = bom.check_account(f'@{user_input}')
+                st.markdown("<hr style='border: 1px solid #33C5FF; border-radius: 2px;'>", unsafe_allow_html=True)
+                st.markdown("<h2 style='color: #33C5FF;'>Botometer Results</h2>", unsafe_allow_html=True)
+                col1, col2 = st.columns([1,1])
+                col1.markdown("<h3 style='text-align: center; color: var(--main-text-color);'>User Scores</h3>",unsafe_allow_html=True)
+                col1.write(result)
+                col2.markdown("<h3 style='text-align: center; color: var(--main-text-color);'>Follower Scores</h3>",unsafe_allow_html=True)
+                col2.write(follower_screen_names_bot_scores)
+
+            except tweepy.TweepError:
+                st.error("Failed to fetch user data. Please check if the handle is valid.")
+        else:
+            st.warning("Please enter a Twitter handle.")
+
+# Set output section
+with output_section:
+    st.markdown("<hr style='border: 1px solid #33C5FF; border-radius: 2px;'>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center; color: var(--secondary-text-color);'>Created with ❤️ by Team 06</p>", unsafe_allow_html=True)
